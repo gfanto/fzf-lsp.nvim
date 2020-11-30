59434958e6b87f8e8859d5ee409d68538a6d0830
Gianmarco Fantinuoli <gianmarco.fanti@gmail.com>
2020-11-30 22:18:55 +0100

    center hightlighted line

--- bin/preview.sh
+++ bin/preview.sh
@@ -2,8 +2,45 @@
 
 set -eu
 
-FILENAME=$(echo $1 | awk -F '|' '{print $1}')
-LNUM=$(echo $1 | awk -F '|' '{print $2}' | awk '{print $1}')
+FILE=$(echo $1 | awk -F '|' '{print $1}')
+CENTER=$(echo $1 | awk -F '|' '{print $2}' | awk '{print $1}')
 
-bat --color=always --style=numbers -r $LNUM: -H $LNUM "$FILENAME" | head -n 1000
+if [[ -n "$CENTER" && ! "$CENTER" =~ ^[0-9] ]]; then
+  exit 1
+fi
+CENTER=${CENTER/[^0-9]*/}
+
+FILE="${FILE/#\~\//$HOME/}"
+if [ ! -r "$FILE" ]; then
+  echo "File not found ${FILE}"
+  exit 1
+fi
+
+if [ -z "$CENTER" ]; then
+  CENTER=0
+fi
+
+UP=$(($CENTER-$LINES/2))
+
+# Sometimes bat is installed as batcat.
+if command -v batcat > /dev/null; then
+  BATNAME="batcat"
+elif command -v bat > /dev/null; then
+  BATNAME="bat"
+fi
+
+UP=$(($CENTER-$LINES/2))
+DOWN=$(($CENTER+$LINES/2))
+
+if [ $UP -lt 0 ]; then
+  if [ $DOWN -lt $LINES ]; then
+    DOWN=$LINES
+  else
+    DOWN=$(($UP+$DOWN))
+  fi
+  UP=0
+fi
+
+${BATNAME} --style="${BAT_STYLE:-numbers}" --color=always --highlight-line=$CENTER \
+  --line-range="$UP:$DOWN" "$FILE"
 
